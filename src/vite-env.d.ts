c8a90e1505fb382f54d600be5820153a26fda5ce
phat@minim1 <phat.tranducvn@gmail.com>
2023-05-11 19:40:01 -0700

    Add assets

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,4 +1,11 @@
 /// <reference types="vite/client" />
+import { ExternalProvider } from "@metamask/providers";
+
+declare global {
+  interface Window {
+      ethereum: ExternalProvider;
+  }
+}
 
 interface ImportMetaEnv {
 	readonly VITE_SEPOLIA_RPC_URL: string
