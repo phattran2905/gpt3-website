037480de88f0fdfd428caac25a2640f99daeaf3f
phat@minim1 <phat.tranducvn@gmail.com>
2023-05-11 19:58:24 -0700

    Add styles for navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,24 @@
 import { useState } from "react"
 import "./App.css"
-import MainMint from './MainMint';
-import Navbar from './Navbar';
+import MainMint from "./MainMint"
+import Navbar from "./Navbar"
 
 function App() {
-  const [accounts, setAccounts] = useState([]);
+	const [accounts, setAccounts] = useState([])
 
 	return (
-		<div className="App">
-			<Navbar accounts={accounts} setAccounts={setAccounts} />
-			<MainMint accounts={accounts} setAccounts={setAccounts} />
+		<div className="overlay">
+			<div className="App">
+				<Navbar
+					accounts={accounts}
+					setAccounts={setAccounts}
+				/>
+				<MainMint
+					accounts={accounts}
+					setAccounts={setAccounts}
+				/>
+			</div>
+			<div className="moving-background"></div>
 		</div>
 	)
 }
--- src/Navbar.tsx
+++ src/Navbar.tsx
@@ -1,5 +1,10 @@
+import { Box, Button, Flex, Image, Link, Spacer } from "@chakra-ui/react"
+import Facebook from "./assets/social-media-icons/facebook_32x32.png"
+import Twitter from "./assets/social-media-icons/twitter_32x32.png"
+import Email from "./assets/social-media-icons/email_32x32.png"
+
 type Props = {
-	accounts: string
+	accounts: never[]
 	setAccounts: (value: any) => void
 }
 
@@ -17,20 +22,74 @@ function Navbar({ accounts, setAccounts }: Props) {
 	}
 
 	return (
-		<div>
+		<Flex
+			justify={"space-between"}
+			align={"center"}
+			padding={"30px"}
+		>
 			{/* Left side - Social Media Icon */}
-			<div>Facebook</div>
-			<div>Twitter</div>
-			<div>Email</div>
+			<Flex
+				justify={"space-around"}
+				width={"40%"}
+				padding={"0px 75px"}
+			>
+				<Link href="https://www.facebook.com">
+					<Image
+						src={Facebook}
+						boxSize={"42px"}
+						margin={"0 15px"}
+					/>
+				</Link>
+				<Link href="https://www.twitter.com">
+					<Image
+						src={Twitter}
+						boxSize={"42px"}
+						margin={"0 15px"}
+					/>
+				</Link>
+				<Link href="https://www.gmail.com">
+					<Image
+						src={Email}
+						boxSize={"42px"}
+						margin={"0 15px"}
+					/>
+				</Link>
+			</Flex>
 
 			{/* Right side - Sections and Connect */}
-			<div>About</div>
-			<div>Mint</div>
-			<div>Team</div>
+			<Flex
+				justify={"space-around"}
+				align={"center"}
+				width={"40%"}
+				padding={"30px"}
+			>
+				<Box margin={"0 15px"}>About</Box>
+				<Spacer />
+				<Box margin={"0 15px"}>Mint</Box>
+				<Spacer />
+				<Box margin={"0 15px"}>Mint</Box>
+				<Spacer />
 
-			{/* Connect */}
-			{isConnected ? <p>Connected</p> : <button onClick={connectAccount}>Connect</button>}
-		</div>
+				{/* Connect */}
+				{isConnected ? (
+					<Box margin={"0 15px"}>Connected</Box>
+				) : (
+					<Button
+						backgroundColor={"#D6517D"}
+						borderRadius={"5px"}
+						boxShadow={"0px 2px 2px 1px #0F0F0F"}
+						color={"white"}
+						cursor={"pointer"}
+						fontFamily={"inherit"}
+						padding="15px"
+						margin={"0 15px"}
+						onClick={connectAccount}
+					>
+						Connect
+					</Button>
+				)}
+			</Flex>
+		</Flex>
 	)
 }
 
